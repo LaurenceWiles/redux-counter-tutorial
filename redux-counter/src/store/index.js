35c3f400821b11f57dfe6f44efe806b76efee8e6
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-05-24 11:24:34 +0200

    removed unnescessary comments

--- redux-counter/src/store/index.js
+++ redux-counter/src/store/index.js
@@ -23,27 +23,3 @@ const store = configureStore({
 export const actions = counterSlice.actions;
 
 export default store;
-
-/*
-const initialState = { counter: 0 };
-
-const reducerFn = (state = initialState, action) => {
-  switch (action.type) {
-    case "INC":
-      return { counter: state.counter + 1 };
-    case "DEC":
-      return { counter: state.counter - 1 };
-    case "ADD":
-      return { counter: state.counter + action.payload };
-    default:
-      return state;
-  }
-};
-
-const store = configureStore({
-  reducer: reducerFn,
-});
-
-export default store;
-
-*/
